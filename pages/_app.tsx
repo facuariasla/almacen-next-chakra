c7160344f8a7af3e98f638d74d02fb44f57a4c7e
facuariasla <facundolautaroarias@hotmail.com>
2022-07-26 11:55:57 -0300

    icon added and title2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 // pages/_app.js
 import React from "react";
-import { Box, ChakraProvider, Container, Divider, Heading, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  ChakraProvider,
+  Container,
+  Divider,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import theme from "../theme";
 import Head from "next/head";
@@ -12,21 +21,24 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <title>Almacen Pepe</title>
       </Head>
       <Box p={4}>
-      <Container
-        background="white"
-        boxShadow="md"
-        maxWidth="container.xl"
-        p={4}
-        borderRadius='sm'
-      >
-        <VStack mb={6}>
-          <Image borderRadius={9999} src="https://placeimg.com/180/180/tech"/>
-          <Heading>Almacen</Heading>
-          <Text>El almacen de Pepe</Text>
-        </VStack>
-        <Divider my={6}/>
-        <Component {...pageProps} />
-      </Container>
+        <Container
+          background="white"
+          boxShadow="md"
+          maxWidth="container.xl"
+          p={4}
+          borderRadius="sm"
+        >
+          <VStack mb={6}>
+            <Image
+              borderRadius={9999}
+              src="https://placeimg.com/180/180/tech"
+            />
+            <Heading>Almacen</Heading>
+            <Text>El almacen de Pepe</Text>
+          </VStack>
+          <Divider my={6} />
+          <Component {...pageProps} />
+        </Container>
       </Box>
     </ChakraProvider>
   );
