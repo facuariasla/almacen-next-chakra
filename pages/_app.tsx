98213ff61f1e15ee6081f4a084a7a73ea27062d1
facuariasla <facundolautaroarias@hotmail.com>
2022-08-25 16:42:47 -0300

    logo changed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import {
   Divider,
   Heading,
   Image,
+  Stack,
   Text,
   VStack,
 } from "@chakra-ui/react";
@@ -28,14 +29,49 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
           p={4}
           borderRadius="sm"
         >
-          <VStack mb={6}>
+          {/* <VStack mb={6}>
             <Image
               borderRadius={9999}
               src="https://placeimg.com/180/180/tech"
             />
             <Heading>Almacen</Heading>
             <Text>El almacen de Pepe</Text>
-          </VStack>
+
+          </VStack> */}
+          <Stack display="flex" w="100%" justify="center" align="center">
+            <Stack
+              border="16px solid red"
+              w="150px"
+              h="150px"
+              borderRadius="50%"
+              borderTopRightRadius={0}
+              borderBottomLeftRadius={0}
+              display="flex"
+              align="center"
+              justify="center"
+            >
+              <Text
+                color="red"
+                fontWeight={700}
+                fontSize={30}
+                m="0"
+                lineHeight={1}
+              >
+                PEPE's
+              </Text>
+              <Text
+                color="red"
+                fontWeight={500}
+                fontSize={12}
+                m="0"
+                lineHeight={1}
+                marginTop="0px"
+              >
+                STORE
+              </Text>
+            </Stack>
+          </Stack>
+
           <Divider my={6} />
           <Component {...pageProps} />
         </Container>
